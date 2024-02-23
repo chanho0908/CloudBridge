0c6bf75a0abc33acbc172c64f937934b61393766
chanho <uni070@naver.com>
2024-02-24 01:05:05 +0900

    BottomNavigation Color 수정

--- app/src/main/java/com/myproject/cloudbridge/view/main/MainActivity.kt
+++ app/src/main/java/com/myproject/cloudbridge/view/main/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.myproject.cloudbridge.view.main
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationBarView
 import com.myproject.cloudbridge.R
@@ -22,6 +25,19 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
         binding.navigationView.setOnItemSelectedListener(this)
         setCurrentFragment(storeListFragment)
+
+        val navigationView = binding.navigationView
+        // 색상 리소스 ID에서 실제 색상 가져오기
+        // 색상 리소스 ID에서 실제 색상 가져오기
+        val colorWhite = ContextCompat.getColor(this, R.color._white)
+
+        // ColorDrawable로 변환하여 Drawable 변수에 할당
+
+        // ColorDrawable로 변환하여 Drawable 변수에 할당
+        val backgroundDrawable: Drawable = ColorDrawable(colorWhite)
+        navigationView.itemBackground = backgroundDrawable
+
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
